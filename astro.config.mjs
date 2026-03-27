f5b191e74ae7580ddfbc6966da94be6620b3e789
Ayo Suber <ayo@Mac.attlocal.net>
2026-03-27 01:24:10 -0700

    set base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://helloayo.github.io',
-    base: '/ayosuber.github.io',
+    site: 'https://ayosuber.github.io',
 });
